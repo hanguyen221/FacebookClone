0085cf4ca246a3b14cff1aff2149e266376e1e6e
Ha Nguyen <thanhha.nguyen2211@gmail.com>
2017-08-26 21:44:57 +0700

    no message

---
+++ FacebookClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookClone
+//
+//  Created by Ha Nguyen on 8/26/17.
+//  Copyright © 2017 Ha Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
